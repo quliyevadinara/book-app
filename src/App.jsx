81a5502f1899b25246790dcb337c15e7e2a5cea4
quliyevadinara <quliyevadinara14@gmail.com>
2023-12-10 15:14:48 +0400

    NavMenu added

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import AboutPage from "./components/AboutPage";
+import BooksPage from "./components/BooksPage";
+import NavMenu from "./components/NavMenu";
+import FooterPage from "./components/FooterPage";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavMenu />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/books" element={<BooksPage />} />
+        </Routes>
+        <FooterPage />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavMenu.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const NavMenu = () => {
+  return (
+    <div className="nav-menu">
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Link to="/" className="navbar-brand" href="#home">E-Book</Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" > 
+            <Nav className="me-auto justify-content-end">
+              <Link to="/" className="nav-link" href="#home">Home</Link >
+              <Link to="/books" className="nav-link" href="#link">Books</Link >
+              <Link to="/about" className="nav-link" href="#link">About</Link >
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavMenu;
