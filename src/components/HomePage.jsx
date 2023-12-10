72e18cac520303c384997658ac7bdd46293f9d83
Asima Shahsuvarli <shahsuvar71@gmail.com>
2023-12-10 19:45:42 +0400

    HomePage completed

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Card from "./Card"
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+      <> 
+    <div className="header">
+      <div className="row1">
+        <h1>A room without books is like<br/> a body without a soul.</h1>
+      </div>
+      <div className="row2">
+        <h2>Find Your Book</h2>
+        <div className="search">
+          <input type="text" placeholder="Enter Your Book Name" />
+          <button><i className="fa-solid fa-magnifying-glass"></i></button>
+        </div>
+        <img src="./images/bg2.png" alt="" />
+      </div>
+    </div>
+      <div className="container">
+      <Card/>
+      </div>
+      </>
+  );
+};
 
-export default HomePage
+export default HomePage;
